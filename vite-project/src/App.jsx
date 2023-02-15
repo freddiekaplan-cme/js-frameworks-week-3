0616c583e948f7c82da91f9672993cbfa2550ee2
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-02-15 14:14:37 +0100

    Getting started. Added pic.

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react'
+import billboard from './assets/billboard.jpg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+     <img className='billboard-background' src={billboard} alt='A billboard in a big city.' />
+     
+    </div>
+  )
+}
+
+export default App
