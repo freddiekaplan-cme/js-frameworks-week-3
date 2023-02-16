b736462a9daf6cb2edc40ac25da13117b58fa59d
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-02-16 18:20:27 +0100

    Looks like mockup in 1200px x 1200px.

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,16 +7,41 @@ function App() {
 
   return (
     <div className="App">
-     <img className='billboard-background' src={billboard} alt='A billboard in a big city.' />
-     <div className='container'>
-      <div className='main-text'>
-        Billboard<br />Mockup
+
+      <img className='billboard-background' src={billboard} alt='A billboard in a big city.' />
+      
+      <div className='container'>
+
+        <div className='main-text'>
+          Billboard<br />Mockup
+        </div>
+
+        <div className='second-text'>
+          Put Your<br />Design Here
+        </div>
+
+        <div className='triangle-container'>
+          <div className='triangle triangle-peach'></div>
+          <div className='triangle triangle-pink'></div>
+          <div className='triangle triangle-peach'></div>
+          <div className='triangle triangle-pink'></div>
+          <div className='triangle triangle-peach'></div>
+
+          <div className='triangle triangle-transparent'></div>
+          <div className='triangle triangle-pink'></div>
+          <div className='triangle triangle-peach'></div>
+          <div className='triangle triangle-pink'></div>
+          <div className='triangle triangle-peach'></div>
+
+          <div className='triangle triangle-transparent'></div>
+          <div className='triangle triangle-transparent'></div>
+          <div className='triangle triangle-pink'></div>
+          <div className='triangle triangle-peach'></div>    
+          <div className='triangle triangle-pink'></div>
+        </div>
+
       </div>
-      <div className='second-text'>
-        Put Your<br />Design Here
-      </div>
-     </div>
-    
+
     </div>
   )
 }
