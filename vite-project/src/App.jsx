ca5f61505f6dac856234195e0b8566fd6c419815
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-02-20 22:15:19 +0100

    Added more media queries, added opacity, refactoring.

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,23 +1,10 @@
 import { useState } from 'react'
+import { nameOne, nameTwo, triangleColors, colorArrayNumbers } from './arrays.js'
+import './assets/styles/App.css'
+import './assets/styles/typography.css'
 import billboard from './assets/billboard.jpg'
-import '../src/typography.css'
-import './App.css'
 
 function App() {
-  
-  //const [classes, setClasses] = useState(triangleColors);
-  
-  const nameOne = ["P", "Lil'", "Big", "Mad", "Div", "Flex", "Grid", "DOM", "Click", "Const", "Absolute", "Var", "Solid", "Root", "Vite", "The", "Script", "Let", "Head", "Body", "Hyper", "String", "Func", "Loopy", "Ol' Dirty", "Biggie", "Vanilla", "MC", "Busta", "Run", "Kool", "Dr.", "Slick", "Fat", "Master", "Notorious", "Repo"]
-  const nameTwo = ["Divvy", "P", "Array", "Div", "Body", "Flexxx", "Cla$$", "Style", "PX", "Link", "Source", "Element",  "Border", "Solid", "Nav X", "DOM", "App", "Selecta", "Block", "Id", "Lang", "Method", "Loop", "Iterator", "Fetch", "For Each", "Git", "A.P.I."]
-  const triangleColors = ['triangle-peach', 'triangle-pink']
-  // const [currentClassPeach, setCurrentClassPeach] = useState(triangleColors[0])
-  // const [currentClassPink, setCurrentClassPink] = useState(triangleColors[1])
-
-  
-  // console.log(colorArrayNumbers)
-  // console.log(randomColorArrayNumbers)
-  
-  const colorArrayNumbers = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
   const [colorArray, changeColorArrayNumbers] = useState(colorArrayNumbers)
   const [currentNameOne, setCurrentNameOne] = useState('Web Dev')
   const [currentNameTwo, setCurrentNameTwo] = useState('Rap Name')
@@ -25,79 +12,35 @@ function App() {
   const changeName = () => {
     const randomNameOne = Math.floor(Math.random() * nameOne.length)
     const randomNameTwo = Math.floor(Math.random() * nameTwo.length)
-    // return nameOne[randomNameOne] & nameTwo[randomNameTwo]
     setCurrentNameOne(nameOne[randomNameOne])
     setCurrentNameTwo(nameTwo[randomNameTwo])
 
     const shuffleColorArrayNumbers = arr => [...arr].sort(() => Math.random() - 0.5)
-  
     const randomColorArrayNumbers = shuffleColorArrayNumbers(colorArrayNumbers)
     changeColorArrayNumbers(randomColorArrayNumbers)
   }
   
-  // const changeColorArray = () => {
-  //   const shuffleColorArrayNumbers = arr => [...arr].sort(() => Math.random() - 0.5)
-  
-  //   const randomColorArrayNumbers = shuffleColorArrayNumbers(colorArrayNumbers)
-  //   changeColorArrayNumbers(randomColorArrayNumbers)
-  // }
+  return (
+    <div className="App">
 
-  // const [currentClassPeach, setCurrentClassPeach] = useState([triangleColors[0], triangleColors[1]])
-  // const elements = useRef([])
-    
-  // const changeClass = () => {
-  //   const newClassPeach = triangleColors.map(() => {
-  //   const randomIndex = Math.floor(Math.random() * triangleColors.length)
-  //   return triangleColors[randomIndex]
-  //   });
-  //   setCurrentClassPeach(newClassPeach)
-  // }
-  
-    // const randomIndexPeach = Math.floor(Math.random() * triangleColors.length) 
-    // const randomClassPeach = randomIndexPeach
-    // setCurrentClassPeach(triangleColors[randomClassPeach])
-    
-    // const randomIndexPink = Math.floor(Math.random() * triangleColors.length) 
-    // const randomClassPink = randomIndexPink
-    // setCurrentClassPink(triangleColors[randomClassPink])
+      <div className='flip-device-container'>
+        <div className='flip-device-yo'>Yo!</div>
+        <div className='flip-device'>The screen is too narrow. Flip your device or make your window larger.</div>
+      </div>
 
-  
-  
-  // const triangleColors = ['triangle-peach', 'triangle-pink']
-  // const [currentClassPeach, setCurrentClassPeach] = useState(triangleColors[0])
- // const [currentClassPink, setCurrentClassPink] = useState(triangleColors[1])
-  
-  // const changeClass = () => {
-    //   const randomIndexPeach = Math.floor(Math.random() * triangleColors.length) 
-    //   const randomClassPeach = randomIndexPeach
-    //   setCurrentClassPeach(triangleColors[randomClassPeach])
-    //   const randomIndexPink = Math.floor(Math.random() * triangleColors.length) 
-    
-    //   const randomClassPink = randomIndexPink
-    //   setCurrentClassPink(triangleColors[randomClassPink])
-    // }
-    
-    return (
-      <div className="App">
-
-      <img className='billboard-background' src={billboard} alt='A billboard in a big city.' />
-      
+      <img className='billboard-background' src={billboard} alt='A billboard in a big city.' />  
       <div className='container'>
 
-        <div className='main-text'>
+        <div className='generated-name'>
           {currentNameOne}
           <br />
           {currentNameTwo}
-          {/* Billboard<br />Mockup */}
         </div>
-        {/* <button onClick={changeName}>Change Class</button>
-        <button onClick={[changeName, changeClass]}>Change Class</button> */}
 
-        <div className='second-text' onClick={changeName}>
+        <div className='click-to-change-name' onClick={changeName}>
           Click Here to
           <br />
           Get Your Name
-          {/* Put Your<br />Design Here */}
         </div>
 
         <div className='triangle-container'>
@@ -119,25 +62,6 @@ function App() {
           <div className={`triangle ${triangleColors[colorArray[10]]}`}></div>    
           <div className={`triangle ${triangleColors[colorArray[11]]}`}></div>
         </div>
-        {/* <div className='triangle-container'>
-          <div className={`triangle ${currentClassPeach[0]}`} ref={el => elements.current[0] = el}></div>
-          <div className={`triangle ${currentClassPeach[1]}`} ref={el => elements.current[1] = el}></div>
-          <div className={`triangle ${currentClassPeach[0]}`} ref={el => elements.current[0] = el}></div>
-          <div className={`triangle ${currentClassPeach[1]}`} ref={el => elements.current[1] = el}></div>
-          <div className={`triangle ${currentClassPeach[0]}`} ref={el => elements.current[0] = el}></div>
-
-          <div className={`triangle triangle-transparent`}></div>
-          <div className={`triangle ${currentClassPeach[1]}`} ref={el => elements.current[1] = el}></div>
-          <div className={`triangle ${currentClassPeach[0]}`} ref={el => elements.current[0] = el}></div>
-          <div className={`triangle ${currentClassPeach[1]}`} ref={el => elements.current[1] = el}></div>
-          <div className={`triangle ${currentClassPeach[0]}`} ref={el => elements.current[0] = el}></div>
-
-          <div className={`triangle triangle-transparent`}></div>
-          <div className={`triangle triangle-transparent`}></div>
-          <div className={`triangle ${currentClassPeach[1]}`} ref={el => elements.current[1] = el}></div>
-          <div className={`triangle ${currentClassPeach[0]}`} ref={el => elements.current[0] = el}></div>    
-          <div className={`triangle ${currentClassPeach[1]}`} ref={el => elements.current[1] = el}></div>
-        </div> */}
 
       </div>
     </div>
---
+++ vite-project/src/arrays.js
@@ -0,0 +1,4 @@
+export const nameOne = ["P", "Lil'", "Big", "Mad", "Div", "Flex", "Grid", "DOM", "Click", "Absolute", "Var", "Solid", "Root", "Vite", "The", "Let", "Head", "Body", "Hyper", "String", "Func", "Loopy", "Ol' Dirty", "Biggie", "Vanilla", "MC", "Busta", "Run", "Kool", "Dr.", "Slick", "Fat", "Master", "Notorious", "Repo"]
+export const nameTwo = ["Divvy", "P", "Array", "Div", "Body", "Flexxx", "Cla$$", "Const", "Style", "PX", "Link", "Source", "Element",  "Border", "Solid", "Script", "Nav X", "DOM", "App", "Selecta", "Block", "Id", "Lang", "Method", "Loop", "Iterator", "Fetch", "For Each", "Git", "A.P.I."]
+export const triangleColors = ['triangle-peach', 'triangle-pink']
+export const colorArrayNumbers = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
